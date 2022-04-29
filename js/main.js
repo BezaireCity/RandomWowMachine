d1185b1a5d8c0d090e030d3dfdb2936bbf75adf9
Dallas Bezaire <102712130+BezaireCity@users.noreply.github.com>
2022-04-29 10:00:55 -0600

    Added basic HTML and API request JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 
+document.querySelector('button').addEventListener('click', getWow)
+
+function getWow(){
+  const url = 'https://owen-wilson-wow-api.herokuapp.com/wows/random'
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
