daf86c097a419ff14b70b233959437e4fad5cb4b
Dallas Bezaire <102712130+BezaireCity@users.noreply.github.com>
2022-04-29 10:50:51 -0600

    added basic CSS style, changed HTML to have more images, and made a JS function to cycle through different Owen heads and place them onto DOM with each click

--- js/main.js
+++ js/main.js
@@ -7,11 +7,35 @@ function getWow(){
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        const wowSound= data.audio
+        const wowSound= data[0].audio
+        console.log(wowSound)
         const audio = new Audio(wowSound);
         audio.play();
+        const owenHead=getHead()
+        document.querySelector('#owenHead').src=owenHead
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
+}
+
+function getHead(){
+    const choose=Math.random()
+    if (choose<0.14){
+        return "img/Owen1.jpg"
+    } else if (choose<0.28) {
+        return "img/Owen2.jpg"
+    } else if (choose<0.28) {
+        return "img/Owen2.jpg"
+    } else if (choose<0.42) {
+        return "img/Owen3.jpg"
+    } else if (choose<0.57) {
+        return "img/Owen4.jpg"
+    } else if (choose<0.71) {
+        return "img/Owen5.jpg"
+    } else if (choose<0.86) {
+        return "img/Owen6.jpg"
+    } else {
+        return "img/Owen7.jpg"
+    }
 }
